2529a244bdab2f9063bc912d1977fa5d6a372538
Jared Malooly <51115098+Jared-Sqeud@users.noreply.github.com>
2019-07-23 03:56:51 -0700

    Added support for v.redd.it to mp4 conversion

--- mayabot.py
+++ mayabot.py
@@ -8,7 +8,7 @@ import urllib.request
 import os
 
 def main():
-    username = 'mayabot'
+    username = 'mayaxs'
     r = praw.Reddit(client_id = '1scCXWF6gu7Ecg',
                     client_secret = 'BcRWHiN-UXTagFSlvjgm6m_zQMg',
                     username = 'Mayabot',
@@ -43,7 +43,7 @@ def get_post_ids(user, r):
         used_ids.append(line.rstrip())
     used_ids_txt.close()
     used_ids_txt = open('used_ids.txt', 'a')
-    for submission in user.submissions.new(limit=20):
+    for submission in user.submissions.new(limit=5):
 
         #time.sleep(5) #For use in final in case the rpi requests too often
 
@@ -54,12 +54,13 @@ def get_post_ids(user, r):
         if submission not in used_ids and sub != "u_" + user.name:
             try:
                 post_ids.append(submission.id)
-                used_ids_txt.write(submission.id + '\n')
+                #used_ids_txt.write(submission.id + '\n')
                 title, link_to_image, id = get_image(submission.id, submission.title) #Fuck me heres the issue
                 #SHOULD key out duplicate posts!
                 new_posts[title] = [link_to_image, id]
             except Exception as e:
-                print(e)
+                print(e, submission.id)
+                print('The post may be deleted or an invalid (text based) post')
 
     if post_ids == []:
         print('All posts accounted for!')
@@ -75,7 +76,7 @@ def get_post_ids(user, r):
             gfycat_download(new_posts, key)
         elif 'external-preview.redd.it' in type:
             imgur_download(new_posts, key)
-        elif 'v.redd.it' in type:
+        elif 'DASH_480?source=fallback.mp4' in type:
             vreddit_download(new_posts, key)
         elif 'gallery' in type:
             extract_gallery(new_posts, key)
@@ -104,8 +105,20 @@ def get_image(id, title):
                     print(link, "is v.redd.it")
                 return title, "/".join(possible_link), id
 
+    for link in soup.find_all("video"):
+        split_div = str(link)
+        split_div = split_div.split(' ')
+        for i in split_div:
+            find_src = i.split('=')
+            if 'src' in find_src:
+                link = find_src[1].strip('"')
+                link = link.strip('HLSPlaylist.m3u8')
+                link = link + 'DASH_480?source=fallback.mp4'
+                return title, link, id
+
+
 def get_video(id, title):
-    pass
+    print('v.redd.it')
 
     #
 
@@ -188,6 +201,14 @@ def ireddit_download(new_posts, key):
     to_upload.close()
 
 def vreddit_download(new_posts, key):
-    print(new_posts)
+
+    link = new_posts[key][0]
+    name = new_posts[key][1] + '.mp4'
+    caption = key
+    urllib.request.urlretrieve(link, 'pics/' + name)
+
+    to_upload = open("img_and_caption.txt", "a")
+    to_upload.write(caption + ' | ' + name + "\n")
+    to_upload.close()
 
 main()
