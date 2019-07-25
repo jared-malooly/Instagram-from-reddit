cd2f6081944c7dce1bef33385a131a8e5fc249db
Jared Malooly <51115098+Jared-Sqeud@users.noreply.github.com>
2019-07-25 06:12:39 -0700

    V1

--- grampost.py
+++ grampost.py
@@ -1,32 +1,60 @@
-from instapy_cli import client
+import InstagramAPI as ig
 import time
-
-
-username = 'fluft_boys'
-password = 'WhySoRude'
-
+import cv2
 
 def main():
 
-    posts = []
-    instructions = open("img_and_caption.txt", "r")
+    username = 'fluft_boys'
+    password = 'WhySoRude'
+    InstagramAPI = ig.InstagramAPI(username, password)
+    instructions = open("img_and_caption.txt", 'r')
 
     for instruction in instructions:
-        instr = instruction.rstrip("\n")
-        posts.append(instr.split(" | "))
+        InstagramAPI.login()  # login
+        instruction = instruction.rstrip('\n')
+        stuff = instruction.split(" | ")
+        file = stuff[1]
+        caption = stuff[0]
+
+        buzzwords = ["wholesome"]
+        words = caption.split(" ")
+        for word in words:
+            if word.lower() in buzzwords:
+                words[words.index(word)] = "#" + word
+        caption = " ".join(words)
 
 
-    for instruction in posts:
-        text = instruction[0]
-        image = 'pics/' + instruction[1]
+        type = file.split(".")
+
+        print(file)
+
         try:
-            with client(username, password) as cli:
-                cli.upload(image, text)
-        except Exception as e:
-            print("Image type not supported yet :(")
-            print(e)
+            if "mp4" in type:
+                print('Attempting to post mp4...')
+                video_path = 'pics/' + file
+                print(video_path)
+                thumbnail = get_thumbnail('pics/' + file, type[0])
+                InstagramAPI.uploadVideo(video_path, thumbnail, caption)
+                print("Made post!")
+
+            elif "jpg" in type:
+                print('Attempting to post jpg...')
+                photo_path = 'pics/' + file
+                print(photo_path)
+                InstagramAPI.uploadPhoto(photo_path, caption)
+                print("Made post!")
+
+        except:
+            print("Something went wrong with posting the image")
+
+        InstagramAPI.logout()
         time.sleep(30)
 
-def run():
+def get_thumbnail(directory, file):
+    vidcap = cv2.VideoCapture(directory)
+    success, image = vidcap.read()
+    cv2.imwrite("pics/" + file + ".jpg", image)
+    return "pics/" + file + ".jpg"
 
+def run():
     main()
